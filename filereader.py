86c77617fcc10cd846dc27d9f913feddf77821d4
shicha1203 <shicha1203@tamu.edu>
2020-07-25 12:42:29 -0500

    Update filereader.py

--- filereader.py
+++ filereader.py
@@ -3,15 +3,15 @@ def passwd_check(check):
     val = True
 
     if len(check) < 8:
-        print('length should be atleast 8 characters long')
+        print(check+' length should be atleast 8 characters long')
         val = False
     
     if len(check) > 64:
-        print('Exceeded maximum length allowed')
+        print(check+' Exceeded maximum length allowed')
         val = False
     
     if not isascii(check):
-        print('does not contain ascii characters')
+        print(check+' does not contain ascii characters')
         val = False
     
     if val:
@@ -24,18 +24,21 @@ def isascii(check):
 def main():
     ##passwd= 'भारत'
 
+    weak_password = open("weak_password.txt", "r")
+
+    read_all = weak_password.read()
     with open("input_password.txt", "r") as fp:
         lines = fp.readlines()
 
         for line in lines:
             check = line.strip()
 
-
-    if(passwd_check(check)):
-        print("password is valid")
-    else:
-        print("password is invalid")
-
+            if(passwd_check(check)):
+                print(check+ " password is valid")
+            elif (line in read_all):
+                print(check+" password is common")
+            else:
+                print(" password is invalid")
 
 
 if __name__ == "__main__":
