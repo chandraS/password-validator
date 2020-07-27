f7ade7422c398a1a9a244142da30a0113e1cd29b
shicha1203 <shicha1203@tamu.edu>
2020-07-27 00:03:29 -0500

    Update const.py

--- password_validator/const.py
+++ password_validator/const.py
@@ -4,7 +4,7 @@ MAX_PASSWORD_LENGTH = 64
 LOWER_ASCII_CODE = 31
 UPPER_ASCII_CODE = 129
 
-INVALID_CHARACTERS = Error: Invalid Characters
-TOO_SHORT = Error: Too Short
-TOO_LONG = Error: Too Long
-TOO_COMMON = Error: Too Common
+INVALID_CHARACTERS = "-> Error: Invalid Characters"
+TOO_SHORT = "{} --> Error: Too Short"
+TOO_LONG = "{} --> Error: Too Long"
+TOO_COMMON = "{} -> Error: Too Common"
